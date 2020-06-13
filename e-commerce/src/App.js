80fc1b6e4f2575d20156c366d4839746ee15a373
jason <jason_doan94@yahoo.com>
2020-06-12 22:51:36 -0400

    product set up

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <Products />
     </div>
   );
 }
---
+++ e-commerce/src/components/Products/Products.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import product1 from "../../images/product-1.jpeg";
+
+class Products extends Component {
+  render() {
+    return (
+      <section className="products">
+        <div className="section-title">
+          <h2>Our Products</h2>
+        </div>
+        <div class="products-center">
+          {/* single product */}
+          <article className="product">
+            <div className="img-container">
+              <img src={product1} alt="product" className="product-img" />
+              <button className="bag-btn" data-id="1">
+                <i className="fas fa-shopping-cart">Add To Bag</i>
+              </button>
+            </div>
+            <h3>Jordan 1 Retro High Off-White University Blue</h3>
+            <h4>$500</h4>
+          </article>
+          {/* single product */}
+          <article className="product">
+            <div className="img-container">
+              <img src={product1} alt="product" className="product-img" />
+              <button className="bag-btn" data-id="1">
+                <i className="fas fa-shopping-cart">Add To Bag</i>
+              </button>
+            </div>
+            <h3>Jordan 1 Retro High Off-White University Blue</h3>
+            <h4>$500</h4>
+          </article>
+          {/* single product */}
+          <article className="product">
+            <div className="img-container">
+              <img src={product1} alt="product" className="product-img" />
+              <button className="bag-btn" data-id="1">
+                <i className="fas fa-shopping-cart">Add To Bag</i>
+              </button>
+            </div>
+            <h3>Jordan 1 Retro High Off-White University Blue</h3>
+            <h4>$500</h4>
+          </article>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Products;
