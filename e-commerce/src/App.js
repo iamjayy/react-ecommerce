1a940c3a67be87eea1f81fcc5eb8ab2f41e882cc
jason <jason_doan94@yahoo.com>
2020-06-17 19:49:25 -0400

    product js

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -5,45 +5,60 @@ import Hero from "./components/Hero/Hero";
 import Items from "./components/Products/Products";
 import Cart from "./components/Cart/Cart";
 //import Main from "./MainJavascript/Main";
-const cartBtn = document.querySelector(".cart-btn");
-const closeCartBtn = document.querySelector(".close-cart");
-const clearCartBtn = document.querySelector(".clear-cart");
-const cartDOM = document.querySelector(".cart");
-const cartOverlay = document.querySelector(".cart-overlay");
-const cartItems = document.querySelector(".cart-items");
-const cartTotal = document.querySelector(".cart-total");
-const cartContent = document.querySelector(".cart-content");
-const productsDom = document.querySelector(".products-center");
 
-//cart
-let cart = [];
+function App() {
+  const cartBtn = document.querySelector(".cart-btn");
+  const closeCartBtn = document.querySelector(".close-cart");
+  const clearCartBtn = document.querySelector(".clear-cart");
+  const cartDOM = document.querySelector(".cart");
+  const cartOverlay = document.querySelector(".cart-overlay");
+  const cartItems = document.querySelector(".cart-items");
+  const cartTotal = document.querySelector(".cart-total");
+  const cartContent = document.querySelector(".cart-content");
+  const productsDom = document.querySelector(".products-center");
 
-//getting products
-class Products {
-  async getProducts() {
-    try {
-      let result = await fetch("products.json");
-      return result;
-    } catch (error) {
-      console.log(error);
+  //cart
+  let cart = [];
+
+  //getting products
+  class Products {
+    async getProducts() {
+      try {
+        let result = await fetch("products.json");
+        let data = await result.json();
+
+        let products = data.items;
+        products = products.map(item => {
+          const { title, price } = item.fields;
+          const { id } = item.sys;
+          const image = item.fields.image.fields.file.url;
+          return { title, price, id, image };
+        });
+        return products;
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
-}
 
-//display products
-class UI {}
+  //display products
+  class UI {
+    displayProducts(products) {
+      console.log(products);
+    }
+  }
 
-//local storage
-class Storage {}
+  //local storage
+  class Storage {}
 
-document.addEventListener("DOMContentLoaded", () => {
-  const ui = new UI();
-  const products = new Products();
+  document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
 
-  //get all products
-  products.getProducts().then(data => console.log(data));
-});
-function App() {
+    //get all products
+    products.getProducts().then(products => ui.displayProducts(products));
+  });
+
   return (
     <div className="App">
       <NavBar />
